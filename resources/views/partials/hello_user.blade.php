c8df37bdbdd4c1eb425d09e4bca1fa4f3ff9733a
LC <lcyoong@creino.com>
2016-11-01 11:37:39 +0800

    Logout url relative to tenant

--- resources/views/partials/hello_user.blade.php
+++ resources/views/partials/hello_user.blade.php
@@ -2,7 +2,7 @@
 <li class="dropdown">
   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">@lang('nav.hello', ['name' => auth()->user()->name])<span class="caret"></span></a>
   <ul class="dropdown-menu">
-    <li><a href="/logout">@lang('nav.logout')</a></li>
+    <li><a href="{{ urlTenant('/logout') }}">@lang('nav.logout')</a></li>
     <li><a href="/settings/account">@lang('nav.settings')</a></li>
     <li><a href="/merchants">@lang('merchant.accounts')</a></li>
   </ul>
