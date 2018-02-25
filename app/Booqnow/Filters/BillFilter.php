7e793ae8a097fc2430360fa96ad870283fa6fb31
LC <lcyoong@creino.com>
2018-02-25 16:33:50 +0800

    Fix search full name with trimming

--- app/Booqnow/Filters/BillFilter.php
+++ app/Booqnow/Filters/BillFilter.php
@@ -13,8 +13,8 @@ class BillFilter extends QueryFilter
   {
     if (!empty($value)) {
 
-      return $this->builder->whereRaw("trim(concat(trim(cus_first_name), ' ', trim(cus_last_name))) like '%$value%'");
-      
+      return $this->builder->where('trim(bil_customer_name)', 'like', "%$value%");
+
     }
   }
 
