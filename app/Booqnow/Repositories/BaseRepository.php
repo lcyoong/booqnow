8c85218d8b5b763903c4fb4a87718f1917514b44
LC <lcyoong@creino.com>
2019-01-17 19:02:41 +0100

    Sort resources by name

--- app/Booqnow/Repositories/BaseRepository.php
+++ app/Booqnow/Repositories/BaseRepository.php
@@ -11,254 +11,231 @@ use Cache;
 
 class BaseRepository implements BaseRepositoryInterface
 {
+    protected $paginate = 10;
 
-  protected $paginate = 10;
+    protected $repo;
 
-  protected $repo;
+    protected $rules = [];
 
-  protected $rules = [];
+    protected $rules_alt = [];
 
-  protected $rules_alt = [];
+    protected $expectsJson = false;
 
-  protected $expectsJson = false;
+    protected $filter;
 
-  protected $filter;
+    protected $update = false;
 
-  protected $update = false;
+    protected $withs = [];
 
-  protected $withs = [];
-
-  /**
-   * Create new repository instance
-   * @param string $class - Model class name of child class
-   */
-  public function __construct($class)
-  {
-    $this->repo = new $class();
-  }
-
-  /**
-   * Get the paginated result of query
-   * @param  Filter $filters - Repository filter
-   * @param  [type] $joins   [description]
-   * @param  string $order   [description]
-   * @return [type]          [description]
-   */
-  public function getPages($filters = null, $order = 'desc')
-  {
-    $resource = $this->repo->select('*');
-
-    if (!is_null($filters)) {
-
-      $resource->filter($filters);
-
+    /**
+     * Create new repository instance
+     * @param string $class - Model class name of child class
+     */
+    public function __construct($class)
+    {
+        $this->repo = new $class();
     }
 
-    if (!is_null($this->filter)) {
+    /**
+     * Get the paginated result of query
+     * @param  Filter $filters - Repository filter
+     * @param  [type] $joins   [description]
+     * @param  string $order   [description]
+     * @return [type]          [description]
+     */
+    public function getPages($filters = null, $order = 'desc', $orderBy = null)
+    {
+        $resource = $this->repo->select('*');
 
-      $resource->filter($this->filter);
+        if (!is_null($filters)) {
+            $resource->filter($filters);
+        }
 
+        if (!is_null($this->filter)) {
+            $resource->filter($this->filter);
+        }
+
+        foreach ($this->withs as $with) {
+            $resource->with($with);
+        }
+
+        $resource->orderBy($orderBy ?? $this->repo->getKeyName(), $order);
+
+        return $resource->paginate($this->paginate);
     }
 
-    foreach ($this->withs as $with) {
+    /**
+     * Get the collection result of query
+     * @param  [type]  $filters [description]
+     * @param  integer $limit   [description]
+     * @return [type]           [description]
+     */
+    public function get($filters = null, $limit = 0, $orders = [])
+    {
+        $resource = $this->repo->select('*');
 
-      $resource->with($with);
+        if (!is_null($filters)) {
+            $resource->filter($filters);
+        }
 
+        if (!is_null($this->filter)) {
+            $resource->filter($this->filter);
+        }
+
+        if ($limit > 0) {
+            $resource->limit($limit);
+        }
+
+        foreach ($this->withs as $with) {
+            $resource->with($with);
+        }
+
+        foreach ($orders as $field => $order) {
+            $resource->orderby($field, $order);
+        }
+
+        return $resource->get();
     }
 
-    $resource->orderBy($this->repo->getKeyName(), $order);
-
-    return $resource->paginate($this->paginate);
-  }
-
-  /**
-   * Get the collection result of query
-   * @param  [type]  $filters [description]
-   * @param  integer $limit   [description]
-   * @return [type]           [description]
-   */
-  public function get($filters = null, $limit = 0, $orders = [])
-  {
-    $resource = $this->repo->select('*');
-
-    if (!is_null($filters)) {
-
-      $resource->filter($filters);
-
+    /**
+     * Get a single repository item by primary key
+     * @param  int $id - Item id
+     * @return Model
+     */
+    public function findById($id)
+    {
+        return $this->repo->with($this->withs)->findOrFail($id);
     }
 
-    if (!is_null($this->filter)) {
-
-      $resource->filter($this->filter);
-
-    }
-
-    if ($limit > 0) {
-
-      $resource->limit($limit);
-
-    }
-
-    foreach ($this->withs as $with) {
-
-      $resource->with($with);
-
-    }
-
-    foreach ($orders as $field => $order) {
-
-      $resource->orderby($field, $order);
-
-    }
-
-    return $resource->get();
-  }
-
-  /**
-   * Get a single repository item by primary key
-   * @param  int $id - Item id
-   * @return Model
-   */
-  public function findById($id)
-  {
-    return $this->repo->with($this->withs)->findOrFail($id);
-  }
-
-  public function first()
-  {
-    $resource = $this->repo->select('*');
+    public function first()
+    {
+        $resource = $this->repo->select('*');
     
-    if (!is_null($this->filter)) {
+        if (!is_null($this->filter)) {
+            $resource->filter($this->filter);
+        }
 
-      $resource->filter($this->filter);
-
+        return $resource->first();
     }
 
-    return $resource->first();
-  }
+    /**
+     * Process storing of new repository item
+     * @param  array $input - input data
+     * @return Model
+     */
+    public function store($input)
+    {
+        $input = array_filter($input);
 
-  /**
-   * Process storing of new repository item
-   * @param  array $input - input data
-   * @return Model
-   */
-  public function store($input)
-  {
-    $input = array_filter($input);
+        $this->validate($input);
 
-    $this->validate($input);
-
-    return $this->repo->create($input);
-  }
-
-  /**
-   * Process updating of repository item
-   * @param  array $input - input data
-   * @return Model
-   */
-  public function update($input)
-  {
-    $resource = $this->repo->findOrFail(array_get($input, $this->repo->getKeyName()));
-
-    $this->validate(array_filter($input + $resource->toArray()));
-
-    return $resource->update($input);
-  }
-
-  /**
-   * Process deleting of repository item by primary key
-   * @param  int $id - Item id
-   * @return boolean
-   */
-  public function deleteById($id)
-  {
-    return $this->repo->find($id)->delete();
-  }
-
-  /**
-   * Validate input data
-   * @param  array $input - input data
-   * @return void
-   */
-  public function validate($input)
-  {
-    $this->update = array_key_exists($this->repo->getKeyName(), $input);
-
-    $validator = Validator::make($input, $this->rules());
-
-    if ($validator->fails()) {
-
-      throw new ValidationException($validator, $validator->messages());
-    }
-  }
-
-  // public function all($columns = ['*'])
-  // {
-  //   return $this->repo->get($columns);
-  // }
-
-  // public function paginate($perPage = 15, $columns = ['*'])
-  // {
-  //   return $this->repo->paginate($perPage, $columns);
-  // }
-
-  public function filter($filters)
-  {
-    // return $this->repo->filter($filters);
-    $this->filter = $filters;
-
-    return $this;
-  }
-
-  /**
-   * Get the key-value array result of a query
-   * @param  string $key - Column name of the array key
-   * @param  string $label - Column name of the array value
-   * @param  string $cache_name - Cache name (optional)
-   * @return array
-   */
-  public function getDropDown($key, $label, $cache_name = null)
-  {
-    $resource = $this->repo->select('*');
-
-    if (!is_null($this->filter)) {
-      $resource->filter($this->filter);
+        return $this->repo->create($input);
     }
 
-    // Cache::flush();
+    /**
+     * Process updating of repository item
+     * @param  array $input - input data
+     * @return Model
+     */
+    public function update($input)
+    {
+        $resource = $this->repo->findOrFail(array_get($input, $this->repo->getKeyName()));
 
-    if (!is_null($cache_name)) {
-      return Cache::remember($cache_name, 90, function() use($resource, $key, $label)
-      {
-        return $resource->toDropDown($key, $label);
-      });
-    } else {
-      return $resource->toDropDown($key, $label);
+        $this->validate(array_filter($input + $resource->toArray()));
+
+        return $resource->update($input);
     }
 
-  }
-
-  public function with($values)
-  {
-    $this->withs = $values;
-
-    return $this;
-  }
-
-  protected function rules()
-  {
-    if ($this->update && method_exists($this, 'alt_rules')) {
-      return call_user_func([$this, 'alt_rules']);
+    /**
+     * Process deleting of repository item by primary key
+     * @param  int $id - Item id
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->repo->find($id)->delete();
     }
 
-    return $this->rules;
-  }
+    /**
+     * Validate input data
+     * @param  array $input - input data
+     * @return void
+     */
+    public function validate($input)
+    {
+        $this->update = array_key_exists($this->repo->getKeyName(), $input);
 
-  public function __call($name, $arguments)
-  {
-    $this->filter->add([$name => $arguments]);
+        $validator = Validator::make($input, $this->rules());
 
-    return $this;
-  }
+        if ($validator->fails()) {
+            throw new ValidationException($validator, $validator->messages());
+        }
+    }
 
+    // public function all($columns = ['*'])
+    // {
+    //   return $this->repo->get($columns);
+    // }
+
+    // public function paginate($perPage = 15, $columns = ['*'])
+    // {
+    //   return $this->repo->paginate($perPage, $columns);
+    // }
+
+    public function filter($filters)
+    {
+        // return $this->repo->filter($filters);
+        $this->filter = $filters;
+
+        return $this;
+    }
+
+    /**
+     * Get the key-value array result of a query
+     * @param  string $key - Column name of the array key
+     * @param  string $label - Column name of the array value
+     * @param  string $cache_name - Cache name (optional)
+     * @return array
+     */
+    public function getDropDown($key, $label, $cache_name = null)
+    {
+        $resource = $this->repo->select('*');
+
+        if (!is_null($this->filter)) {
+            $resource->filter($this->filter);
+        }
+
+        // Cache::flush();
+
+        if (!is_null($cache_name)) {
+            return Cache::remember($cache_name, 90, function () use ($resource, $key, $label) {
+                return $resource->toDropDown($key, $label);
+            });
+        } else {
+            return $resource->toDropDown($key, $label);
+        }
+    }
+
+    public function with($values)
+    {
+        $this->withs = $values;
+
+        return $this;
+    }
+
+    protected function rules()
+    {
+        if ($this->update && method_exists($this, 'alt_rules')) {
+            return call_user_func([$this, 'alt_rules']);
+        }
+
+        return $this->rules;
+    }
+
+    public function __call($name, $arguments)
+    {
+        $this->filter->add([$name => $arguments]);
+
+        return $this;
+    }
 }
--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -12,112 +12,111 @@ use Filters\ResourceFilter;
 
 class ResourceController extends MainController
 {
-  protected $repo_rs;
+    protected $repo_rs;
 
-  /**
-   * Create a new controller instance.
-   * @param ResourceRepository $repo_rs
-   */
-  public function __construct(ResourceRepository $repo_rs)
-  {
-    parent::__construct();
+    /**
+     * Create a new controller instance.
+     * @param ResourceRepository $repo_rs
+     */
+    public function __construct(ResourceRepository $repo_rs)
+    {
+        parent::__construct();
 
-    $this->tenant = true;
+        $this->tenant = true;
 
-    $this->layout = 'layouts.tenant';
+        $this->layout = 'layouts.tenant';
 
-    $this->repo_rs = $repo_rs;
-  }
+        $this->repo_rs = $repo_rs;
+    }
 
-  /**
-   * Display resources list
-   * @param  Request $request
-   * @param  int  $rty_id - Resource type id
-   * @return Response
-   */
-  public function index(Request $request, $rty_id)
-  {
-    $resource_type = (new ResourceTypeRepository)->findById($rty_id);
+    /**
+     * Display resources list
+     * @param  Request $request
+     * @param  int  $rty_id - Resource type id
+     * @return Response
+     */
+    public function index(Request $request, $rty_id)
+    {
+        $resource_type = (new ResourceTypeRepository)->findById($rty_id);
 
-    $filters = new ResourceFilter($request->input() + ['type' => $resource_type->rty_id]);
+        $filters = new ResourceFilter($request->input() + ['type' => $resource_type->rty_id]);
 
-    $filter = $request->input();
+        $filter = $request->input();
 
-    $this->page_title = trans('resource.list', ['type' => $resource_type->rty_plural]);
+        $this->page_title = trans('resource.list', ['type' => $resource_type->rty_plural]);
 
-    $this->new_path = urlTenant(sprintf('resources/%s/new', $resource_type->rty_id));
+        $this->new_path = urlTenant(sprintf('resources/%s/new', $resource_type->rty_id));
 
-    $resources = $this->repo_rs->getPages($filters, 'asc');
+        $resources = $this->repo_rs->getPages($filters, 'asc', 'rs_name');
 
-    $this->vdata(compact('resources', 'filter', 'resource_type'));
+        $this->vdata(compact('resources', 'filter', 'resource_type'));
 
-    return view('resource.list', $this->vdata);
-  }
+        return view('resource.list', $this->vdata);
+    }
 
-  /**
-   * Display the new resource form
-   * @param  int $rty_id - Resource type id
-   * @return Response
-   */
-  public function create($rty_id)
-  {
-    $sub_types = ResourceSubType::getDropDown($rty_id);
+    /**
+     * Display the new resource form
+     * @param  int $rty_id - Resource type id
+     * @return Response
+     */
+    public function create($rty_id)
+    {
+        $sub_types = ResourceSubType::getDropDown($rty_id);
 
-    $resource_type = (new ResourceTypeRepository)->findById($rty_id);
+        $resource_type = (new ResourceTypeRepository)->findById($rty_id);
 
-    $this->page_title = trans('resource.new', ['type' => $resource_type->rty_name]);
+        $this->page_title = trans('resource.new', ['type' => $resource_type->rty_name]);
 
-    $this->vdata(compact('resource_type', 'sub_types'));
+        $this->vdata(compact('resource_type', 'sub_types'));
 
-    return view('resource.new', $this->vdata);
-  }
+        return view('resource.new', $this->vdata);
+    }
 
-  /**
-   * Display the edit resource form
-   * @param  int  $rs_id - Resource id
-   * @return Response
-   */
-  public function edit($rs_id)
-  {
-    $resource = $this->repo_rs->findById($rs_id);
+    /**
+     * Display the edit resource form
+     * @param  int  $rs_id - Resource id
+     * @return Response
+     */
+    public function edit($rs_id)
+    {
+        $resource = $this->repo_rs->findById($rs_id);
 
-    $sub_types = ResourceSubType::getDropDown($resource->rs_type);
+        $sub_types = ResourceSubType::getDropDown($resource->rs_type);
 
-    $resource_type = (new ResourceTypeRepository)->findById($resource->rs_type);
+        $resource_type = (new ResourceTypeRepository)->findById($resource->rs_type);
 
-    $this->page_title = trans('resource.edit', ['type' => $resource_type->rty_name]);
+        $this->page_title = trans('resource.edit', ['type' => $resource_type->rty_name]);
 
-    $this->vdata(compact('resource', 'resource_type', 'sub_types'));
+        $this->vdata(compact('resource', 'resource_type', 'sub_types'));
 
-    return view('resource.edit', $this->vdata);
-  }
+        return view('resource.edit', $this->vdata);
+    }
 
-  /**
-   * Process storing of new resource
-   * @param  Request $request
-   * @return Response
-   */
-  public function store(Request $request)
-  {
-    $input = $request->input();
+    /**
+     * Process storing of new resource
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->input();
 
-    $this->repo_rs->store($input);
+        $this->repo_rs->store($input);
 
-    return $this->goodReponse();
-  }
+        return $this->goodReponse();
+    }
 
-  /**
-   * Process updating of a resource
-   * @param  Request $request
-   * @return Response
-   */
-  public function update(Request $request)
-  {
-    $input = $request->input();
+    /**
+     * Process updating of a resource
+     * @param  Request $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        $input = $request->input();
 
-    $this->repo_rs->update($input);
+        $this->repo_rs->update($input);
 
-    return $this->goodReponse();
-  }
-
+        return $this->goodReponse();
+    }
 }
