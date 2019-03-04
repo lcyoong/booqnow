f74f0ce90dc5885e7758a7e903ac302cd40e06cd
LC <lcyoong@creino.com>
2019-03-04 21:29:07 +0100

    Tweak occupancy report by day of month - include confirmed and remove free

--- app/Booqnow/Reports/Excel/OccupancyByDayExcel.php
+++ app/Booqnow/Reports/Excel/OccupancyByDayExcel.php
@@ -14,163 +14,140 @@ use Log;
 
 class OccupancyByDayExcel extends ExcelReport
 {
-  protected $year;
+    protected $year;
 
-  protected $total_rooms;
+    protected $total_rooms;
 
-  protected $occ_arr, $occ_day;
+    protected $occ_arr;
+    protected $occ_day;
 
-  public function __construct($report)
-  {
-    parent::__construct($report->rep_function);
+    public function __construct($report)
+    {
+        parent::__construct($report->rep_function);
 
-    extract(unserialize($report->rep_filter));
+        extract(unserialize($report->rep_filter));
 
-    $this->year = $year;
-  }
-
-  /**
-   * Handle report generation
-   * @return void
-   */
-  public function handle()
-  {
-    Excel::create($this->reportname, function($excel) {
-
-      $excel->sheet('Sheet1', function($sheet) {
-
-        $this->sheet = $sheet;
-
-        $this->setting();
-
-        $this->header();
-
-        $this->getData();
-
-        $this->occupancy();
-
-        $this->footer();
-
-  		});
-		})->store($this->ext);
-  }
-
-  /**
-   * Report header
-   * @return void
-   */
-  protected function header()
-  {
-    // title
-    $this->sheet->getStyle("A1")->getFont()->setSize(18);
-
-    $this->fillRow([trans('report.daily_occupancy_report_heading', ['ext' => $this->year])]);
-
-    // Header
-    $row = ['Month\Day'];
-
-    for ($day = 1; $day <= 31; $day++) {
-
-      $row[] = $day;
-
+        $this->year = $year;
     }
 
-    $row[] = 'Total';
+    /**
+     * Handle report generation
+     * @return void
+     */
+    public function handle()
+    {
+        Excel::create($this->reportname, function ($excel) {
+            $excel->sheet('Sheet1', function ($sheet) {
+                $this->sheet = $sheet;
 
-    $this->fillRow($row, 1);
-  }
+                $this->setting();
 
-  /**
-   * Report income section
-   * @return void
-   */
-  protected function occupancy()
-  {
-    for ($mth = 1; $mth <= 12; $mth++) {
+                $this->header();
 
-      $row = [Carbon::createFromDate(null, $mth, 1)->format('M')];
+                $this->getData();
 
-      for ($day = 1; $day <= 31; $day++) {
+                $this->occupancy();
 
-        $row[$day] = 0;
+                $this->footer();
+            });
+        })->store($this->ext);
+    }
 
-      }
+    /**
+     * Report header
+     * @return void
+     */
+    protected function header()
+    {
+        // title
+        $this->sheet->getStyle("A1")->getFont()->setSize(18);
 
-      if (isset($this->occ_arr[$mth])) {
+        $this->fillRow([trans('report.daily_occupancy_report_heading', ['ext' => $this->year])]);
 
-        foreach ($this->occ_arr[$mth] as $day => $counter) {
+        // Header
+        $row = ['Month\Day'];
 
-          $row[$day] = $counter;
-
+        for ($day = 1; $day <= 31; $day++) {
+            $row[] = $day;
         }
 
-      }
+        $row[] = 'Total';
 
-      $row[] = array_sum($row);
+        $this->fillRow($row, 1);
+    }
 
-      $this->fillRow($row, 0);
+    /**
+     * Report income section
+     * @return void
+     */
+    protected function occupancy()
+    {
+        for ($mth = 1; $mth <= 12; $mth++) {
+            $row = [Carbon::createFromDate(null, $mth, 1)->format('M')];
 
-      // Percentage
-      $row = ["%"];
+            for ($day = 1; $day <= 31; $day++) {
+                $row[$day] = 0;
+            }
 
-      for ($day = 1; $day <= 31; $day++) {
+            if (isset($this->occ_arr[$mth])) {
+                foreach ($this->occ_arr[$mth] as $day => $counter) {
+                    $row[$day] = $counter;
+                }
+            }
 
-        $row[$day] = 0;
+            $row[] = array_sum($row);
 
-      }
+            $this->fillRow($row, 0);
 
-      if (isset($this->occ_arr[$mth])) {
+            // Percentage
+            $row = ["%"];
 
-        foreach ($this->occ_arr[$mth] as $day => $counter) {
+            for ($day = 1; $day <= 31; $day++) {
+                $row[$day] = 0;
+            }
 
-          $row[$day] = number_format($counter*100/$this->total_rooms) . '%';
+            if (isset($this->occ_arr[$mth])) {
+                foreach ($this->occ_arr[$mth] as $day => $counter) {
+                    $row[$day] = number_format($counter*100/$this->total_rooms) . '%';
+                }
+            }
 
+            $this->fillRow($row, 0);
         }
 
-      }
+        // Summary
+        $row = ["Total rooms occupied"];
 
-      $this->fillRow($row, 0);
+        for ($day = 1; $day <= 31; $day++) {
+            $row[$day] = array_get($this->occ_day, $day, 0);
+        }
 
+        $row[] = array_sum($row);
+
+        $this->fillRow($row, 0);
     }
 
-    // Summary
-    $row = ["Total rooms occupied"];
+    /**
+     * Get the data for report
+     * @return void
+     */
+    protected function getData()
+    {
+        // $occupancies = (new RoomOccupancy)->withoutLabel(['tent'])->byDayOfMonth($this->year);
+        $occupancies = (new RoomOccupancyRepository)->withoutLabel(['tent', 'free'])->byDayOfMonth($this->year);
 
-    for ($day = 1; $day <= 31; $day++) {
+        $this->total_rooms = Resource::whereIn('rs_type', [1])->where('rs_status', '=', 'active')->whereNotIn('rs_label', ['tent'])->count();
 
-      $row[$day] = array_get($this->occ_day, $day, 0);
+        $this->occ_arr = [];
 
+        foreach ($occupancies as $occupancy) {
+            $this->occ_arr[$occupancy->mth][$occupancy->day] = $occupancy->counter;
+
+            if (array_get($this->occ_day, $occupancy->day)) {
+                $this->occ_day[$occupancy->day] += $occupancy->counter;
+            } else {
+                $this->occ_day[$occupancy->day] = $occupancy->counter;
+            }
+        }
     }
-
-    $row[] = array_sum($row);
-
-    $this->fillRow($row, 0);
-  }
-
-  /**
-   * Get the data for report
-   * @return void
-   */
-  protected function getData()
-  {
-    // $occupancies = (new RoomOccupancy)->withoutLabel(['tent'])->byDayOfMonth($this->year);
-    $occupancies = (new RoomOccupancyRepository)->withoutLabel(['tent'])->byDayOfMonth($this->year);
-
-    $this->total_rooms = Resource::whereIn('rs_type', [1])->where('rs_status', '=', 'active')->whereNotIn('rs_label', ['tent'])->count();
-
-    $this->occ_arr = [];
-
-    foreach ($occupancies as $occupancy) {
-
-      $this->occ_arr[$occupancy->mth][$occupancy->day] = $occupancy->counter;
-
-      if (array_get($this->occ_day, $occupancy->day)) {
-        $this->occ_day[$occupancy->day] += $occupancy->counter;
-      } else {
-        $this->occ_day[$occupancy->day] = $occupancy->counter;
-      }
-
-    }
-  }
-
 }
--- app/Booqnow/Repositories/RoomOccupancyRepository.php
+++ app/Booqnow/Repositories/RoomOccupancyRepository.php
@@ -6,46 +6,46 @@ use Illuminate\Http\Request;
 use Filters\RoomOccupancyFilter;
 use DB;
 
-class RoomOccupancyRepository extends BaseRepository {
+class RoomOccupancyRepository extends BaseRepository
+{
 
   /**
    * Create a new repository instance.
    */
-  public function __construct()
-  {
-    parent::__construct('App\RoomOccupancy');
+    public function __construct()
+    {
+        parent::__construct('App\RoomOccupancy');
 
-    $this->filter = new RoomOccupancyFilter();
-  }
+        $this->filter = new RoomOccupancyFilter();
+    }
 
-  public function byDayOfMonth($year)
-  {
-    $this->ofBookStatus(['checkedin', 'checkedout'])->ofYear($year);
+    public function byDayOfMonth($year)
+    {
+        $this->ofBookStatus(['checkedin', 'checkedout', 'confirmed'])->ofYear($year);
 
-    return $this->repo->select(DB::raw("month(ro_date) as mth, day(ro_date) as day, count(*) as counter"))
+        return $this->repo->select(DB::raw("month(ro_date) as mth, day(ro_date) as day, count(*) as counter"))
                 ->filter($this->filter)
                 ->groupBy(DB::raw("month(ro_date), day(ro_date)"))->get();
-  }
+    }
 
-  public function byNational($year)
-  {
-    $this->ofBookStatus(['checkedin', 'checkedout'])->ofYear($year);
+    public function byNational($year)
+    {
+        $this->ofBookStatus(['checkedin', 'checkedout'])->ofYear($year);
 
-    $this->filter->addJoins('joinBookings');
-    $this->filter->addJoins('joinCustomers');
+        $this->filter->addJoins('joinBookings');
+        $this->filter->addJoins('joinCustomers');
 
-    return $this->repo->select(DB::raw("month(ro_date) as mth, cus_country as country, count(*) as counter"))
+        return $this->repo->select(DB::raw("month(ro_date) as mth, cus_country as country, count(*) as counter"))
                 ->filter($this->filter)
                 ->groupBy(DB::raw("month(ro_date), cus_country"))->get();
-  }
+    }
 
-  public function byMonth($year)
-  {
-    $this->ofBookStatus(['checkedin', 'checkedout'])->ofYear($year);
+    public function byMonth($year)
+    {
+        $this->ofBookStatus(['checkedin', 'checkedout'])->ofYear($year);
 
-    return $this->repo->select(DB::raw("month(ro_date) as mth, count(*) as counter"))
+        return $this->repo->select(DB::raw("month(ro_date) as mth, count(*) as counter"))
                 ->filter($this->filter)
                 ->groupBy(DB::raw("month(ro_date)"))->get();
-  }
-
+    }
 }
