37203e1abf0c2957c6355e045880631dfe650d5f
LC <lcyoong@creino.com>
2019-03-18 18:41:45 +0100

    Show spending on occupancy report by sources

--- app/Booqnow/Reports/Excel/OccupancyBySourceExcel.php
+++ app/Booqnow/Reports/Excel/OccupancyBySourceExcel.php
@@ -6,16 +6,18 @@ use App\Events\ReportCreated;
 use DB;
 use Excel;
 use App\Bill;
+use App\BillItem;
 use Carbon\Carbon;
 use Repositories\ResourceRepository;
+use Repositories\BookingSourceRepository;
 use App\RoomOccupancy;
 
 class OccupancyBySourceExcel extends ExcelReport
 {
     protected $year;
     protected $total_rooms;
-
     protected $occ_arr;
+    protected $sources;
 
     public function __construct($report)
     {
@@ -79,8 +81,19 @@ class OccupancyBySourceExcel extends ExcelReport
      */
     protected function occupancy()
     {
+        $this->sheet->setColumnFormat(array(
+            'B1:O100' => '#,##'
+        ));
+      
         foreach ($this->occ_arr as $source => $month_counter) {
-            $row = [$source];
+            $this->sheet->row($this->getRow(), function ($row) {
+
+                // call cell manipulation methods
+                $row->setBackground('#dddddd');
+            });
+
+            $row = [array_get($this->sources, $source)];
+
             $total_days = 0;
 
             for ($month = 1; $month <= 12; $month++) {
@@ -91,8 +104,8 @@ class OccupancyBySourceExcel extends ExcelReport
             foreach ($month_counter as $month => $counter) {
                 if (!empty($month)) {
                     $total_days += $row[$month*2 - 1] = $counter;
-                    $dt = Carbon::createFromDate($this->year, $month, 1);
-                    $percent = number_format($counter / $dt->daysInMonth * 100, 1);
+                    // $dt = Carbon::createFromDate($this->year, $month, 1);
+                    // $percent = number_format($counter / $dt->daysInMonth * 100, 1);
                     // $row[$month*2] = "$percent%";
                 }
             }
@@ -101,6 +114,29 @@ class OccupancyBySourceExcel extends ExcelReport
             $row[] = $total_days;
 
             $this->fillRow($row);
+
+            // Show spending
+            $spending = $this->getSpending($source);
+
+            foreach ($spending as $type => $spend_counter) {
+                $sub_row = [ucfirst($type)];
+
+                $sum_spending = 0;
+
+                for ($month = 1; $month <= 12; $month++) {
+                    $sub_row[$month*2 - 1] = 0;
+                }
+
+                foreach ($spend_counter as $month => $counter) {
+                    if (!empty($month)) {
+                        $sum_spending += $sub_row[$month*2 - 1] = $counter;
+                    }
+                }
+    
+                $sub_row[] = $sum_spending;
+    
+                $this->fillRow($sub_row);
+            }
         }
 
         // Summary row
@@ -140,19 +176,47 @@ class OccupancyBySourceExcel extends ExcelReport
     protected function getData()
     {
         // $occupancies = (new ResourceRepository)->occupancyByRoom($this->year);
-        $occupancies = RoomOccupancy::select(DB::raw("bs_description, month(ro_date) as mth, count(*) as counter"))
+        $this->sources = (new BookingSourceRepository)->getDropDown('bs_id', 'bs_description');
+
+        $occupancies = RoomOccupancy::select(DB::raw("book_source, month(ro_date) as mth, count(*) as counter"))
                         ->join('resources', 'ro_room', '=', 'rs_id')
                         ->join('bookings', 'book_id', '=', 'ro_booking')
-                        ->join('booking_sources', 'bs_id', '=', 'book_source')
+                        // ->join('booking_sources', 'bs_id', '=', 'book_source')
                         ->whereNotIn('rs_label', ['tent', 'bed', 'free'])
                         ->where('rs_type', '=', 1)->whereRaw("year(ro_date) = {$this->year}")
                         ->whereIn('book_status', ['checkedin', 'checkedout','confirmed'])
-                        ->groupBy(DB::raw("bs_description, month(ro_date)"))->get();
+                        ->groupBy(DB::raw("book_source, month(ro_date)"))->get();
+
 
         $this->occ_arr = [];
 
         foreach ($occupancies as $occupancy) {
-            $this->occ_arr[$occupancy->bs_description][$occupancy->mth] = $occupancy->counter;
+            $this->occ_arr[$occupancy->book_source][$occupancy->mth] = $occupancy->counter;
         }
+    }
+
+    private function getSpending($source)
+    {
+        $spendings = BillItem::select(DB::raw("rty_code, month(bil_date) as mth, sum(bili_gross) as total"))
+                        ->join('bills', 'bil_id', '=', 'bili_bill')
+                        ->join('resources', 'rs_id', '=', 'bili_resource')
+                        ->join('resource_types', 'rty_id', '=', 'rs_type')
+                        ->join('bookings', 'book_id', '=', 'bil_booking')
+                        ->where('bili_active', '=', 1)
+                        ->where('book_source', '=', $source)
+                        ->where('bil_status', '=', 'active')
+                        ->where(function ($query) {
+                            $query->whereNotIn('book_status', ['cancelled', 'hold'])->orWhereNull('book_status');
+                        })
+                        ->whereYear('bil_date', $this->year)
+                        ->groupBy(DB::raw("rty_code, month(bil_date)"))->get();
+        
+        $spend_arr = [];
+
+        foreach ($spendings as $spending) {
+            $spend_arr[$spending->rty_code][$spending->mth] = $spending->total;
+        }
+
+        return $spend_arr;
     }
 }
--- app/Booqnow/Reports/ExcelReport.php
+++ app/Booqnow/Reports/ExcelReport.php
@@ -1,78 +1,85 @@
 <?php
 
 namespace Reports;
+
 use Contracts\ReportInterface;
 use App\Traits\ReportTrait;
 use Carbon\Carbon;
 
 abstract class ExcelReport implements ReportInterface
 {
-  use ReportTrait;
+    use ReportTrait;
 
-  protected $sheet;
+    protected $sheet;
 
-  protected $ext = 'xlsx';
+    protected $ext = 'xlsx';
 
-  protected $row = 1;
+    protected $row = 1;
 
-  protected $filters;
+    protected $filters;
 
-  public function __construct($reportname)
-  {
-    $this->reportname = $reportname;
+    public function __construct($reportname)
+    {
+        $this->reportname = $reportname;
 
-    $this->rename();
-  }
-
-  abstract function handle();
-
-  abstract protected function header();
-
-  /**
-   * Rename the report
-   * @return void
-   */
-  public function rename()
-  {
-    $this->reportname = $this->reportname  . "-" . Carbon::now()->format('YmdHis');
-  }
-
-  protected function fillRow($content, $leading_rows = 0, $indent = 0)
-  {
-    $this->row += $leading_rows;
-
-    if ($indent > 0) {
-      $content = array_pad($content, -1 * ($indent + count($content)), '');
+        $this->rename();
     }
 
-    $this->sheet->row($this->row, $content);
+    abstract public function handle();
 
-    $this->row ++;
-  }
+    abstract protected function header();
 
-  /**
-   * Report footer
-   * @return void
-   */
-  protected function footer()
-  {
-    $this->fillRow([session('merchant')->mer_name], 1);
+    /**
+     * Rename the report
+     * @return void
+     */
+    public function rename()
+    {
+        $this->reportname = $this->reportname  . "-" . Carbon::now()->format('YmdHis');
+    }
 
-    $this->fillRow(['Generated: ' . date('d-m-Y H:i:s')]);
+    public function getRow()
+    {
+        return $this->row;
+    }
 
-    $this->fillRow(['**End of report**']);
-  }
+    protected function fillRow($content, $leading_rows = 0, $indent = 0)
+    {
+        $this->row += $leading_rows;
 
-  /**
-   * Report sheet setting
-   * @return void
-   */
-  protected function setting()
-  {
-    $this->sheet->setStyle (
+        if ($indent > 0) {
+            $content = array_pad($content, -1 * ($indent + count($content)), '');
+        }
+
+        $this->sheet->row($this->row, $content);
+
+        $this->row ++;
+    }
+  
+
+    /**
+     * Report footer
+     * @return void
+     */
+    protected function footer()
+    {
+        $this->fillRow([session('merchant')->mer_name], 1);
+
+        $this->fillRow(['Generated: ' . date('d-m-Y H:i:s')]);
+
+        $this->fillRow(['**End of report**']);
+    }
+
+    /**
+     * Report sheet setting
+     * @return void
+     */
+    protected function setting()
+    {
+        $this->sheet->setStyle(
       ['font' => ['name' => 'Calibri', 'size' => 12]]
     );
 
-    $this->sheet->setWidth('A', 35);
-  }
+        $this->sheet->setWidth('A', 35);
+    }
 }
