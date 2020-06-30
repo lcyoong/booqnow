b0c380ba9e301342164543487888412e20501a56
LC <lcyoong@creino.com>
2020-06-30 20:15:59 +0800

    Resources API - order active resources by order field

--- app/Http/Controllers/Api/ResourceApiController.php
+++ app/Http/Controllers/Api/ResourceApiController.php
@@ -2,165 +2,155 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\ResourceSubType;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
+// use App\ResourceType;
+use Repositories\ResourceMaintenanceRepository;
+use Repositories\ResourcePricingRepository;
 use Repositories\ResourceRepository;
 use Repositories\ResourceTypeRepository;
-use Repositories\ResourcePricingRepository;
-use Repositories\ResourceMaintenanceRepository;
-// use App\ResourceType;
-use Filters\ResourceFilter;
-use Filters\ResourceMaintenanceFilter;
-use Carbon\Carbon;
-use App\ResourceSubType;
 
 class ResourceApiController extends ApiController
 {
-  /**
-   * Get the active resources given the parameters
-   * @param  int $rty_id - Resource type id
-   * @return array
-   */
-  public function active($rty_id, $mode = '')
-  {
-    $resource_type = (new ResourceTypeRepository)->findById($rty_id);
-
-    $list = (new ResourceRepository)->ofStatus('active')->ofType($resource_type->rty_id)->get();
-
-    $return = [];
-
-    if ($mode == 'select') {
-      foreach ($list as $item)
-      {
-        $return[] = ['id' => $item->rs_id, 'text' => $item->rs_name, 'price' => (int) $item->rs_price];
-      }
-    } else {
-      foreach ($list as $item)
-      {
-        $return[] = ['id' => $item->rs_id, 'title' => $item->rs_name, 'price' => showMoney($item->rs_price)];
-      }
-    }
-
-    return $return;
-  }
-
-  /**
-   * Get the active resources given the parameters
-   * @param  int $rty_id - Resource type id
-   * @return array
-   */
-  public function activeGrouped($rty_id)
-  {
-    $resource_type = (new ResourceTypeRepository)->findById($rty_id);
-
-    $list = (new ResourceRepository)->ofStatus('active')->ofType($resource_type->rty_id)->orderBy('rs_sub_type', 'asc')->get();
-
-    $return = [];
-
-    foreach ($list as $item)
+    /**
+     * Get the active resources given the parameters
+     * @param  int $rty_id - Resource type id
+     * @return array
+     */
+    public function active($rty_id, $mode = '')
     {
-      $return[!is_null($item->rs_sub_type) ? $item->rs_sub_type : 0][] = ['id' => $item->rs_id, 'text' => $item->rs_name, 'price' => (int) $item->rs_price];
+        $resource_type = (new ResourceTypeRepository)->findById($rty_id);
+
+        $list = (new ResourceRepository)->ofStatus('active')->ofType($resource_type->rty_id)->get(null, null, ['rs_order' => 'asc']);
+
+        $return = [];
+
+        if ($mode == 'select') {
+            foreach ($list as $item) {
+                $return[] = ['id' => $item->rs_id, 'text' => $item->rs_name, 'price' => (int) $item->rs_price];
+            }
+        } else {
+            foreach ($list as $item) {
+                $return[] = ['id' => $item->rs_id, 'title' => $item->rs_name, 'price' => showMoney($item->rs_price)];
+            }
+        }
+
+        return $return;
     }
 
-    return $return;
-  }
-
-  /**
-   * Get the list of resource maintenance given the parameters
-   * @param  Request $request
-   * @return array
-   */
-  public function maintenance(Request $request)
-  {
-    $input = $request->input();
-
-    $list = (new ResourceMaintenanceRepository)->start(array($input, 'start'))->end(array($input, 'end'))->get();
-
-    $return = [];
-
-    foreach ($list as $item)
+    /**
+     * Get the active resources given the parameters
+     * @param  int $rty_id - Resource type id
+     * @return array
+     */
+    public function activeGrouped($rty_id)
     {
-      $return[] = [
-        'type' => 'maintenance',
-        'id' => $item->rm_id,
-        'title' => $item->rm_description,
-        'start' => $item->rm_from,
-        'end' => $item->rm_to,
-        'resourceId' => $item->rm_resource,
-        'status' => $item->rm_status,
-        'backgroundColor' => '#FF0000',
-        'borderColor' => 'transparent',
-      ];
+        $resource_type = (new ResourceTypeRepository)->findById($rty_id);
+
+        $list = (new ResourceRepository)->ofStatus('active')->ofType($resource_type->rty_id)->orderBy('rs_sub_type', 'asc')->get();
+
+        $return = [];
+
+        foreach ($list as $item) {
+            $return[!is_null($item->rs_sub_type) ? $item->rs_sub_type : 0][] = ['id' => $item->rs_id, 'text' => $item->rs_name, 'price' => (int) $item->rs_price];
+        }
+
+        return $return;
     }
 
-    return $return;
-  }
-
-  public function pricing($resource, Request $request)
-  {
-    $rs = new ResourceRepository;
-
-    return $rs->findById($resource)->pricing()->with(['season', 'tiers'])->get();
-  }
-
-  public function ofLabel($label, Request $request)
-  {
-    $rs = new ResourceRepository;
-
-    return $rs->ofLabel($label)->first();
-  }
-
-  public function pricingTier($pricing_id, Request $request)
-  {
-    return (new ResourcePricingRepository)->findById($pricing_id)->tiers()->get();
-  }
-
-  public function selected($resource_id, $start, $end, Request $request)
-  {
-    $nights = Carbon::parse($start)->diffInDays(Carbon::parse($end));
-
-    $rs = new ResourceRepository;
-
-    $resource = $rs->with(['pricing.season'])->findById($resource_id);
-
-    $days = [];
-
-    foreach ($resource->pricing as $rate)
+    /**
+     * Get the list of resource maintenance given the parameters
+     * @param  Request $request
+     * @return array
+     */
+    public function maintenance(Request $request)
     {
-      $overlap = datesOverlap($rate->season->sea_from, $rate->season->sea_to, $start, $end);
+        $input = $request->input();
 
-      if ($overlap > 0) {
-        $days[] = [
-          'price' => (int) $rate->rpr_price,
-          'nights' => $overlap,
-          'description' => $resource->rs_name
-        ];
-      }
+        $list = (new ResourceMaintenanceRepository)->start(array($input, 'start'))->end(array($input, 'end'))->get();
+
+        $return = [];
+
+        foreach ($list as $item) {
+            $return[] = [
+                'type' => 'maintenance',
+                'id' => $item->rm_id,
+                'title' => $item->rm_description,
+                'start' => $item->rm_from,
+                'end' => $item->rm_to,
+                'resourceId' => $item->rm_resource,
+                'status' => $item->rm_status,
+                'backgroundColor' => '#FF0000',
+                'borderColor' => 'transparent',
+            ];
+        }
+
+        return $return;
     }
 
-    if ($nights > array_sum(array_column($days, 'nights'))) {
-      $days[] = [
-        'price' => (int) $resource->rs_price,
-        'nights' => $nights - array_sum(array_column($days, 'nights')),
-        'description' => $resource->rs_name
-      ];
+    public function pricing($resource, Request $request)
+    {
+        $rs = new ResourceRepository;
+
+        return $rs->findById($resource)->pricing()->with(['season', 'tiers'])->get();
     }
 
-    return collect(['resource' => $resource, 'days' => $days]);
-  }
+    public function ofLabel($label, Request $request)
+    {
+        $rs = new ResourceRepository;
 
-  public function types()
-  {
-    return (new ResourceTypeRepository)->get();
-  }
+        return $rs->ofLabel($label)->first();
+    }
 
-  public function subTypes($rty_id)
-  {
-    // dd(ResourceSubType::getDropDown($rty_id));
-    return ResourceSubType::getDropDown($rty_id);
-    // return ResourceSubType::where('rsty_type', '=', $rty_id)->where('rsty_status', '=', 'active')->get();
-  }
+    public function pricingTier($pricing_id, Request $request)
+    {
+        return (new ResourcePricingRepository)->findById($pricing_id)->tiers()->get();
+    }
+
+    public function selected($resource_id, $start, $end, Request $request)
+    {
+        $nights = Carbon::parse($start)->diffInDays(Carbon::parse($end));
+
+        $rs = new ResourceRepository;
+
+        $resource = $rs->with(['pricing.season'])->findById($resource_id);
+
+        $days = [];
+
+        foreach ($resource->pricing as $rate) {
+            $overlap = datesOverlap($rate->season->sea_from, $rate->season->sea_to, $start, $end);
+
+            if ($overlap > 0) {
+                $days[] = [
+                    'price' => (int) $rate->rpr_price,
+                    'nights' => $overlap,
+                    'description' => $resource->rs_name,
+                ];
+            }
+        }
+
+        if ($nights > array_sum(array_column($days, 'nights'))) {
+            $days[] = [
+                'price' => (int) $resource->rs_price,
+                'nights' => $nights - array_sum(array_column($days, 'nights')),
+                'description' => $resource->rs_name,
+            ];
+        }
+
+        return collect(['resource' => $resource, 'days' => $days]);
+    }
+
+    public function types()
+    {
+        return (new ResourceTypeRepository)->get();
+    }
+
+    public function subTypes($rty_id)
+    {
+        // dd(ResourceSubType::getDropDown($rty_id));
+        return ResourceSubType::getDropDown($rty_id);
+        // return ResourceSubType::where('rsty_type', '=', $rty_id)->where('rsty_status', '=', 'active')->get();
+    }
 
 }
