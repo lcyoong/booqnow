adfe6e6973f96e72584b4ad667e6446ff9336161
LC Yoong <lcyoong@LCs-MacBook-Pro.local>
2022-06-22 21:32:28 +0800

    Remove unused links

--- resources/views/partials/hello_user.blade.php
+++ resources/views/partials/hello_user.blade.php
@@ -3,8 +3,8 @@
   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">@lang('nav.hello', ['name' => Auth::user()->name])<span class="caret"></span></a>
   <ul class="dropdown-menu">
     <li><a href="{{ urlTenant('/logout') }}">@lang('nav.logout')</a></li>
-    <li><a href="/settings/account">@lang('nav.settings')</a></li>
-    <li><a href="/merchants">@lang('merchant.accounts')</a></li>
+    <!-- <li><a href="/settings/account">@lang('nav.settings')</a></li>
+    <li><a href="/merchants">@lang('merchant.accounts')</a></li> -->
   </ul>
 </li>
 @endif
